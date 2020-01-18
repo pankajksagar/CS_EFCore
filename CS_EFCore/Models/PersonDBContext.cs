b443e0118d9598451db889dd2234751ad12a83d9
admin <admin@SOFTEDGEIT.COM>
2020-01-18 14:00:16 +0530

    Add project files.

---
+++ CS_EFCore/Models/PersonDBContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_EFCore.Models
+{
+    public class PersonDBContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public PersonDBContext()
+        {
+
+        }
+
+        /// <summary>
+        /// configured db connection with application
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAB1-5; initial Catalog=PersonInfoDB; integrated Security = SSPI;");
+        }
+
+        /// <summary>
+        /// provide mechanisam of model mapping while database and tables are created.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder); 
+            modelBuilder.Entity<Person>();
+        }
+    }
+}
---
+++ CS_EFCore/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace CS_EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            try
+            {
+                ManageDatabase();
+                using (var ctx = new Models.PersonDBContext())
+                {
+                    var person = new Models.Person()
+                    {
+                        //PersonID = 1,
+                        PersonName = "Pankaj",
+                        Gender = "M",
+                        Age = 36
+                    };
+
+                    ctx.Persons.Add(person);
+                    ctx.SaveChanges();
+
+                    Console.WriteLine("Added Person");
+
+                    foreach (var p in ctx.Persons.ToList())
+                    {
+                        Console.WriteLine($"{p.PersonID} {p.PersonName} {p.Gender} {p.Age}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// This method will make sure that if database is already available then delete it.
+        /// </summary>
+        static void ManageDatabase()
+        {
+            using (var ctx = new Models.PersonDBContext())
+            {
+                ctx.Database.EnsureDeleted();
+                ctx.Database.EnsureCreated();
+            }
+        }
+    }
+}
